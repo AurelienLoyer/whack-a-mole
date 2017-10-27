534e40fa10afc29be0cfbcfedfd5f1b76a23cef2
Aurélien Loyer <aur.loy@gmail.com>
2017-10-27 18:53:56 +0200

    feat(front): add vuejs front

---
+++ front/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+<div class="home">
+  coucou
+</div>
+</template>
+
+<script>
+import env from 'env'
+
+export default {
+  name: 'home',
+  data () {
+    return {
+    }
+  },
+  sockets:{
+  },
+}
+</script>
+
+<style lang="css">
+</style>
