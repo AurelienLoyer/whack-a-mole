16d6284548fe11ddb3fc432f5ae1fcfdcea46b6a
Aurélien Loyer <aur.loy@gmail.com>
2017-10-30 11:52:56 +0100

    feat(firebase): move ws to firebase

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const env = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
-const config = require('./config/config.' + env + '.js')
+const config = require('./config/config.js')
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
-const WebSocketServer = require('websocket').server;
+const firebase = require('firebase');
 const port = config.PORT
-const base_uri = config.BASE_URI
 
 // list of currently connected clients (users)
 const clients = []
 
-console.log(`Server Run / Mode ${env} / Port ${port}`)
+console.log(`Server Run / Port ${port}`)
 
 app.use(express.static('front'));
 
@@ -42,38 +40,12 @@ app.get('/consultants', function (req, res) {
 });
 
 /**
- * WebSocket server
+ * Firebase
  */
 
-wsServer = new WebSocketServer({
-  httpServer: server,
-  autoAcceptConnections: false
-});
+console.log(config.FIREBASE_API_KEY);
 
-wsServer.on('request', function (request) {
-  console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
-
-  var connection = request.accept(null, request.origin);
-  var index = clients.push(connection) - 1;
-
-  console.log((new Date()) + ' Connection accepted.');
-  console.log((new Date()) + ' Clients connected : ' + clients.length);
-
-  // send back scores ...
-
-  connection.send(JSON.stringify({ type: 'scores', data: '12-12-19' }));
-  connection.send(JSON.stringify({ type: 'consultants', data: 'moi-toi-lui' }));
-
-  // client sent some message
-  connection.on('message', function (message) {
-    console.log(`message : ${message}`)
-  });
-
-  // user disconnected
-  connection.on('close', function (connection) {
-    console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
-    // remove user from the list of connected clients
-    clients.splice(index, 1);
-  });
-
+firebase.initializeApp({
+  apiKey: config.FIREBASE_API_KEY,
+  databaseURL: `https://${config.FIREBASE_ID}.firebaseio.com`
 });
---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  PORT: process.env.APP_PORT|| 1337,
+  FIREBASE_ID: process.env.FIREBASE_ID|| 'zwhackamole',
+  FIREBASE_API_KEY: process.env.FIREBASE_API_KEY || 'yourapikey',
+}
