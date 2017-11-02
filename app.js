eac3f26bd9677ee873fe5c8880b645c1efd91a7c
Aurélien Loyer <aur.loy@gmail.com>
2017-11-02 13:26:59 +0100

    feat(keypress) listen keyboard keypress for simulate button

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const firebase = require('firebase');
+const WebSocketServer = require('websocket').server;
+const keypress = require('keypress');
 const port = config.PORT
 
 // list of currently connected clients (users)
 const clients = []
+const keymap = [
+  'a','z','e',
+  'q','s','d',
+  'w','x','c',
+]
 
 console.log(`Server Run / Port ${port}`)
 
@@ -22,6 +29,26 @@ app.get('/', function (req, res, next) {
   res.sendfile(`${__dirname}/front/index.html`);
   app.use(express.static(`${__dirname}/front`));
 });
+
+/**
+ * Listen Keypress for dev
+ */
+
+keypress(process.stdin);
+
+process.stdin.on('keypress', function (ch, key) {
+  if(key) {
+    let index = keymap.indexOf(key.name) + 1
+    console.log(index)
+  }
+  if (key && key.ctrl && key.name == 'c') {
+    process.stdin.pause();
+    process.exit();
+  }
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
 
 /**
  * HTTP server
@@ -43,9 +70,48 @@ app.get('/consultants', function (req, res) {
  * Firebase
  */
 
-console.log(config.FIREBASE_API_KEY);
-
 firebase.initializeApp({
   apiKey: config.FIREBASE_API_KEY,
   databaseURL: `https://${config.FIREBASE_ID}.firebaseio.com`
+});
+
+const zenikien = firebase.database().ref('zenikien');
+zenikien.on("value", function(snapshot) {
+  console.log(snapshot.val())
+});
+
+/**
+ * WebSocket server
+ */
+
+wsServer = new WebSocketServer({
+  httpServer: server,
+  autoAcceptConnections: false
+});
+
+wsServer.on('request', function (request) {
+  console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+
+  var connection = request.accept(null, request.origin);
+  var index = clients.push(connection) - 1;
+
+  console.log((new Date()) + ' Connection accepted.');
+  console.log((new Date()) + ' Clients connected : ' + clients.length);
+
+  // send back scores ...
+  connection.send(JSON.stringify({ type: 'scores', data: [] }));
+  connection.send(JSON.stringify({ type: 'zenikien', data: [] }));
+
+  // client sent some message
+  connection.on('message', function (message) {
+    console.log(`message : ${message}`)
+  });
+
+  // user disconnected
+  connection.on('close', function (connection) {
+    console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
+    // remove user from the list of connected clients
+    clients.splice(index, 1);
+  });
+
 });
