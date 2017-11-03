6d1ce7c5393fa2b245fa2c357cdaa9b042968272
Aurélien Loyer <aur.loy@gmail.com>
2017-11-03 17:26:28 +0100

    feat(front): create end game static page

--- front/src/components/End.vue
+++ front/src/components/End.vue
@@ -1,75 +1,39 @@
 <template>
     <div class="end">
-        <div class="result">
-            <h1>NomSuperLongEst.L</h1>
-            <h2>Votre score pour cette partie !</h2>
-            <div class="score">
-                <div class="number">2400</div>
-                <div class="rubban">zenika points</div>
+        <div class="header">
+            <div class="particules"></div>
+            <div class="header-content">
+                <h1>Magdalena.D</h1>
+                <h2>Votre score pour cette partie !</h2>
+                <div class="score">
+                    <div class="number">2400</div>
+                    <div class="rubban">zenika points</div>
+                </div>
             </div>
         </div>
         <div class="content">
-            <ul class="scores">
-                <li>
-                    <span class="num">
-                        <span class="fa-stack fa-lg">
-                            <i class="fa fa-circle fa-stack-2x"></i>
-                            <i class="fa fa-trophy fa-stack-1x fa-inverse"></i>
-                        </span>
-                    </span>
-                    <span class="res">2500</span>
-                    <span class="who">Aurelien.L</span>
-                </li>
-                <li>
-                    <span class="num">2</span>
-                    <span class="res">2400</span>
-                    <span class="who">NomSuperLongEst.L</span>
-                    <span class="you">< VOUS ></span>
-                </li>
-                <li>
-                    <span class="num">3</span>
-                    <span class="res">1300</span>
-                    <span class="who">Aurelien.L</span>
-                </li>
-            </ul>
-
-            <div class="grey">
-                <h3>Consultants</h3>
-                <ul>
-                    <li>
-                        <div class="circle">
-                            <img src="../../statics/img/Zenika-6.jpg"/>
-                        </div>
-                        <span class="name">Aurelien.L</span>
-                        <span class="hit">131</span>
-                    </li>
-                    <li>
-                        <div class="circle">
-                            <img src="../../statics/img/Zenika-7.jpg"/>
-                        </div>
-                        <span class="name">Aurelien.L</span>
-                        <span class="hit">90</span>
-                    </li>
-                    <li>
-                        <div class="circle">
-                            <img src="../../statics/img/Zenika-8.jpg"/>
-                        </div>
-                        <span class="name">Aurelien.L</span>
-                        <span class="hit">30</span>
-                    </li>
-                </ul>
-            </div>
+            <scores></scores>
+            <zenikien></zenikien>
         </div>
 
         <div class="again" @click="$router.push('/')">
-            Play Again
+            Rejouer
         </div>
     </div>
 </template>
 
 <script>
+import Footer from "./Footer.vue";
+import Scores from "./Scores.vue";
+import Zenikien from "./Zenikien.vue";
+
 export default {
-  name: "endgame"
+  name: "endgame",
+  components: {
+    "v-footer": Footer,
+    Scores,
+    Zenikien
+  }
 };
 </script>
 
@@ -78,22 +42,66 @@ export default {
   width: 100%;
 }
 
-.again{
-    color:white;
-    background: red;
-    width: 20%;
-    margin: 50px auto;
-    font-size: 20px;
-    padding:5px 20px;
-    cursor: pointer;
+.again {
+  color: white;
+  background: #c8293e;
+  width: 150px;
+  height: 150px;
+  margin: 50px auto 10px;
+  font-size: 20px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  font-size: 2em;
+  text-transform: uppercase;
+  transition: all 0.2s;
+  &:hover {
+    transform: scale(0.9);
+  }
 }
 
-.result {
+.header {
   background: #22282a;
   color: white;
   padding: 2%;
+
+  .particules {
+    background-image: url(https://www.zenika.com/assets/particles/grey.1d53b2992bf8ef7250a46eb11eb74d2e.svg),
+      url(https://www.zenika.com/assets/particles/red.5f0007f26b124dc527cfe49790bd899c.svg),
+      url(https://www.zenika.com/assets/particles/red-flipped.1affc3e2771bd14f67fa6d6f7cfeadba.svg);
+    background-size: auto 18vw;
+    background-position: 50% 50%, 30% 50%, 70% 50%;
+    background-repeat: no-repeat;
+    height: 325px;
+    position: absolute;
+    width: 100%;
+    opacity: 0.4;
+    animation: move 4s infinite;
+    z-index: 0;
+    top: 0;
+    right: 0;
+    left: 0;
+  }
+
+  @keyframes move {
+    0% {
+    }
+    50% {
+      background-position: 50% 40%, 30% 30%, 70% 60%;
+    }
+    100% {
+    }
+  }
+
+  .header-content {
+    z-index: 4;
+    position: relative;
+  }
+
   .score {
-    width: 10%;
+    width: 170px;
     margin: auto;
     .number {
       background: #fbb831;
@@ -115,86 +123,13 @@ export default {
   display: flex;
   justify-content: space-around;
   padding-top: 2%;
+  width: 100%;
 
-  .scores {
-    width:40%;
-    margin: 0;
-    list-style: none;
-    font-size: large;
-    li {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      padding-right: 20px;
-      position: relative;
-      &:nth-child(1) {
-        background: #feedc8;
-        .res {
-          color: #52bcb5;
-          font-weight: bold;
-          font-size: 1.3em;
-        }
-      }
-      .num {
-        margin-left: -21px;
-        width: 42px;
-        height: 42px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        .fa-circle {
-          color: #fbb831;
-        }
-      }
-      .res {
-        display: block;
-        width: 35%;
-        text-align: right;
-      }
-      .who {
-        width: 40%;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        text-align: right;
-        overflow: hidden;
-      }
-      .you {
-        position: absolute;
-        right: -100px;
-        font-size: 13px;
-        color: white;
-        background: red;
-        padding: 3px 12px;
-        font-weight: bold;
-      }
-    }
+  & > *:nth-child(1) {
+    width: 40%;
   }
-  .grey {
-    background: #f3f3f3;
-    width:20%;
-    min-height: 200px;
-    ul {
-      list-style: none;
-      margin: 0;
-      list-style: none;
-      padding: 4px 20px;
-      li {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding-bottom: 5px;
-        margin-bottom: 5px;
-        .circle {
-          border-radius: 50%;
-          height: 50px;
-          width: 50px;
-          overflow: hidden;
-          img {
-            width: 100%;
-          }
-        }
-      }
-    }
+  & > *:nth-child(2) {
+    width: 20%;
   }
 }
 </style>
